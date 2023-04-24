4b71e898c1dc5a52d02bc774d69c66d7433690c0
Nagabhushanam <nagabhushanam.n@revature.com>
2023-04-24 15:06:16 +0530

    transfer-service with SOLID principles

---
+++ transfer-service/src/main/java/com/example/repository/JpaAccountRepository.java
@@ -0,0 +1,26 @@
+package com.example.repository;
+
+import com.example.model.Account;
+import org.apache.log4j.Logger;
+
+public class JpaAccountRepository implements AccountRepository {
+
+    private static Logger logger=Logger.getLogger("transfer-service");
+
+    public JpaAccountRepository() {
+        logger.info("JpaAccountRepository component instance created");
+    }
+
+    public Account loadAccount(String number){
+        logger.info("loadAccount-"+number);
+        // db code
+        return new Account(number,1000.00);
+    }
+
+    public void updateAccount(Account account){
+        logger.info("updateAccount-"+account.getNumber());
+        // db code
+    }
+
+
+}
